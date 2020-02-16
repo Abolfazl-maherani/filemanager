a08dc3e1f718ef23bd9158b054bd8d42c5810bcb
Abolfazl Maherani <59861215+Abolfazl-maherani@users.noreply.github.com>
2020-02-16 10:54:23 -0800

    end address bar
    create section and end address bar

---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/style/style.css">
+    <link rel="stylesheet" href="assets/style/normalize.css">
+    <link rel="stylesheet" href="assets/style/responsive.css">
+    <title>filemanager</title>
+</head>
+<body>
+<header>
+    <div class="container">
+        <nav>
+            <!--menu-->
+            <div class="span-md-4 menu_line">
+                <div class="three_line">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                </div>
+            </div>
+            <!--search-->
+            <div class="span-md-4 search">
+                <form action="search.php" method="get">
+                    <input type="text" name="search" class="inpt_search" placeholder="جستجو">
+                </form>
+            </div>
+            <!--user account-->
+            <div class="span-md-4 log_info">
+                <div class="right_log">
+                    <div class="toggle_color"></div>
+                </div>
+                <div class="img_log">
+                    <a href="#" class="d_f"><img src="assets/ico/user.svg" alt="" class="profile_user"></a>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
+    <section class="d_f">
+        <div class="container d_f">
+            <div class="span-md-12 body_file">
+                <div class="header_address  span-md-12">
+                    <a href="#" class="ico_bar"><img class="ico_bar" src="assets/ico/home-run.svg" alt=""></a>
+                    <div class="address_bar d_f">
+                    <span>\</span>
+                    <span><a href="#">xampp</a></span>
+                    <span><a href="#">\htdocs</a></span>
+                    <span>\blog_new</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
